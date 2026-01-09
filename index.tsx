d9c1268a8031cf6b9b8c6b218e1ac64fd243f789
Omar Karim <omarkarim9@gmail.com>
2026-01-09 16:47:44 +0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -13,4 +14,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
