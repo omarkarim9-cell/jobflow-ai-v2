c8feca04614c96152115e6fa910f711be6889b68
Omar Karim <omarkarim9@gmail.com>
2026-01-09 18:37:04 +0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-console.log("JobFlow Repair Tool: v1.0.8 Mounting...");
+console.log("JobFlow Repair Tool: v1.0.9 Starting Mount...");
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  console.error("Failed to find the root element. Ensure index.html has <div id='root'></div>");
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  console.log("JobFlow Repair Tool: v1.0.9 Successfully Mounted.");
+}
