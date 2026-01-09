c51395497ac8c04e22adbecb19d454f083ecd189
Omar Karim <omarkarim9@gmail.com>
2026-01-09 17:05:28 +0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("Critical Error: Root element '#root' not found.");
 }
 
 const root = ReactDOM.createRoot(rootElement);
