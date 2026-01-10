b38572b119b055a3d3806fd6b2045eb1feb8f800
Omar Karim <omarkarim9@gmail.com>
2026-01-10 11:36:54 +0400

    Update App.tsx

--- components/App.tsx
+++ components/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { analyzeSyncIssue } from '../services/geminiService.ts';
+import { analyzeSyncIssue } from '../services/geminiService';
 
 // --- INLINED COMPONENTS ---
 
@@ -156,6 +156,7 @@ const App = () => {
 };
 
 export default App;
+
 
 
 
